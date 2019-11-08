3584513420478a8d8cfdec1d83f0a31857a98e18
Princewill Iroka <princewilliroka@outlook.com>
2019-11-08 23:56:30 +0100

    Added travis wait command

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect
 const server = require('../index')
 
 describe('test', () => {
-  it('should return a string', done => {
+  it('should return a string', function(done) {
     expect('ci with travis').to.equal('ci with travis')
     done()
   })
