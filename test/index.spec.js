ec333f400429521030a657cbf2f510c02bd560b7
Princewill Iroka <princewilliroka@outlook.com>
2019-11-12 00:31:37 +0100

    feature(create-user-endpoint) creates employees

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,34 @@
-const expect = require('chai').expect
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
 const server = require('../index')
 
-describe('test', () => {
-  it('should return a string', function(done) {
-    expect('ci with travis').to.equal('ci with travis')
-    done()
+chai.use(chaiHttp)
+
+describe('Teamwork', () => {
+  describe('/POST create-user', () => {
+    it('it should create an employee user account', done => {
+      const data = {
+        firstName: 'Steve',
+        lastName: 'Jobs',
+        email: 'jobs@gmail.com',
+        password: 'pass',
+        gender: 'Male',
+        jobRole: 'Software Developer',
+        department: 'Information Technology',
+        address: 'Ikoyi Street, Lagos',
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9iYW1hQGdtYWlsLmNvbSIsImlhdCI6MTU3MzUxNDc3MSwiZXhwIjoxNTczNTE1MDcxfQ.o-fLKF1uZdKx297DvNr8G1q9T40QMi1K5UkZEf4GZXk'
+      }
+      chai
+        .request(server)
+        .post('/api/v1/auth/create-user')
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.have.property('status').eql('success')
+          done()
+        })
+    })
   })
 })
