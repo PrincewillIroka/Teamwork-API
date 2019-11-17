73b15d06b868f5e51c606ff257a657f10a0103be
Princewill Iroka <princewilliroka@outlook.com>
2019-11-17 10:37:19 +0100

    feature(view-a-specific-gif)

--- src/routes/gifs.route.js
+++ src/routes/gifs.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createGif, deleteGif, commentOnGif } = require('../controllers/GifsController')
+const { createGif, deleteGif, commentOnGif, getGif } = require('../controllers/GifsController')
 
 router.post('/', createGif);
 router.delete('/:gifId', deleteGif);
 router.post('/:gifId/comment', commentOnGif)
+router.get('/:gifId', getGif);
 
 
 module.exports = router;
